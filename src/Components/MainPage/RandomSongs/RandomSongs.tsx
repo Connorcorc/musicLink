48b126e8e3f72bfaee7c8e7be263dc1f9354b1fd
Connor Corcoran <connorcorc@gmail.com>
2022-11-03 11:26:14 -0600

    Fix player + randomSongs

--- src/Components/MainPage/RandomSongs/RandomSongs.tsx
+++ src/Components/MainPage/RandomSongs/RandomSongs.tsx
@@ -1,1 +1,18 @@
-export {}
+import React from "react"
+import './RandomSongs.css'
+
+const RandomSongs = ({  artist, title, audio}) => {
+  return (
+    <div className="player">
+      <h3>{artist}</h3>
+      <p>{title}</p>
+      <video controls className="media">
+        <source src={audio} type="audio/mpeg"></source>
+      </video>
+
+    </div>
+  )
+}
+
+export default RandomSongs
+
